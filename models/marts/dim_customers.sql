f987590a1a2b48397e9c642c3342bce97fd50286
Gabriella Amorim <gabriella.mbaufrj@gmail.com>
2026-07-16 00:11:21 +0000

    Enriquecimento da tabela dim_customers

--- models/marts/dim_customers.sql
+++ models/marts/dim_customers.sql
@@ -1,6 +1,13 @@
 select
-    customer_id,
-    first_name,
-    last_name,
-    first_name || ' ' || last_name as full_name
-from {{ ref('stg_customers') }}
+    c.customer_id,
+    c.first_name,
+    c.last_name,
+    c.first_name || ' ' || c.last_name as full_name,
+    count(o.order_id) as number_of_orders,
+    min(o.order_date) as first_order_date,
+    max(o.order_date) as most_recent_order_date,
+    coalesce(sum(o.amount), 0) as lifetime_value
+from {{ ref('stg_customers') }} as c
+left join {{ ref('fct_orders') }} as o
+    on c.customer_id = o.customer_id
+group by 1, 2, 3, 4
